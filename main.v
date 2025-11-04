7fd3797ad6a42c4eff7adc1adb0f3f526be38c4d
dacmarcell <dacmarcell@gmail.com>
2025-11-03 22:54:14 -0300

    feat: short url and decode url

--- main.v
+++ main.v
@@ -2,6 +2,8 @@ module main
 
 import xiusin.vredis
 import veb
+import time
+import json
 
 pub struct Context {
 	veb.Context
@@ -9,32 +11,82 @@ pub struct Context {
 
 pub struct App {
 	pub:
-		key string
+		domain string
 }
 
-pub fn (app &App) index(mut ctx Context) veb.Result {
-	return ctx.text('Hello from veb $app.key')
+pub struct ShortenResponse {
+	pub:
+		shortened_url string
+		error		  string
 }
 
-fn main() {
+pub struct Url {
+	pub:
+		original_url  string
+		shortened_url string
+}
+
+pub fn (app &App) shorten(mut ctx Context) veb.Result {
+	original_url := ctx.query['url'] or {
+		return ctx.json(ShortenResponse{
+			shortened_url: ''
+			error: 'Missing url parameter'
+		})
+	}
+
+	short_id := time.now().unix().str()
+	shortened_url := app.domain + short_id
+
+	urls := json.encode(Url{
+		shortened_url: shortened_url
+		original_url: original_url
+	})
+
 	mut redis := vredis.new_client(host: '127.0.0.1', port: 6379) or {
-		panic(err)
+	 	panic(err)
 	}
 
-	redis.set("mykey", "hello") or {
-		panic(err)
+	redis.set(short_id, urls) or {
+	 	panic(err)
 	}
-
-	result := redis.get("mykey") or {
-		panic(err)
-	}
-
-	mut app := &App{
-		key: result
-	}
-	veb.run[App, Context](mut app, 8000)
 
 	redis.close() or {
 		panic(err)
 	}
+	
+	return ctx.json(ShortenResponse{
+		shortened_url: shortened_url
+	})
+}
+
+@['/:short_id']
+pub fn (app &App) redirect_url(mut ctx Context, short_id string) veb.Result {
+	mut redis := vredis.new_client(host: '127.0.0.1', port: 6379) or {
+	 	panic(err)
+	}
+
+	url := redis.get(short_id) or {
+	 	panic(err)
+	}
+
+	redis.close() or {
+		panic(err)
+	}
+
+	parsed_url := json.decode(Url, url) or {
+		panic(err)
+	}
+
+	return ctx.redirect(parsed_url.original_url)
+}
+
+pub fn (app &App) index(mut ctx Context) veb.Result {
+	return ctx.text('Hello from veb $app.domain')
+}
+
+fn main() {
+	mut app := &App{
+		domain: 'http://localhost:8000/'
+	}
+	veb.run[App, Context](mut app, 8000)
 }
