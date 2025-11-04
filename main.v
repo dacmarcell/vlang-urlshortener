21f83e3b5d4cf580d9d63a1203f4f4aa7d82fa8e
dacmarcell <dacmarcell@gmail.com>
2025-11-03 23:22:03 -0300

    refactor: create unique redis instance to prevent code duplication

--- main.v
+++ main.v
@@ -12,6 +12,8 @@ pub struct Context {
 pub struct App {
 	pub:
 		domain string
+	mut:
+		redis vredis.Redis
 }
 
 pub struct ShortenResponse {
@@ -26,7 +28,7 @@ pub struct Url {
 		shortened_url string
 }
 
-pub fn (app &App) shorten(mut ctx Context) veb.Result {
+pub fn (mut app App) shorten(mut ctx Context) veb.Result {
 	original_url := ctx.query['url'] or {
 		return ctx.json(ShortenResponse{
         	error: 'Invalid URL format'
@@ -41,46 +43,22 @@ pub fn (app &App) shorten(mut ctx Context) veb.Result {
 		original_url: original_url
 	})
 
-	mut redis := vredis.new_client(host: '127.0.0.1', port: 6379) or {
-	 	return ctx.json(ShortenResponse{
-        	error: 'Redis failed to connect: $err'
-    	})
-	}
-
-	redis.set(short_id, urls) or {
+	app.redis.set(short_id, urls) or {
 	 	return ctx.json(ShortenResponse{
 			error: 'Failed to store URL in Redis: $err'
 		})
 	}
 
-	redis.close() or {
-		return ctx.json(ShortenResponse{
-			error: 'Failed to close Redis connection: $err'
-		})
-	}
-	
 	return ctx.json(ShortenResponse{
 		shortened_url: shortened_url
 	})
 }
 
 @['/:short_id']
-pub fn (app &App) redirect_url(mut ctx Context, short_id string) veb.Result {
-	mut redis := vredis.new_client(host: '127.0.0.1', port: 6379) or {
-		return ctx.json(ShortenResponse{
-        	error: 'Redis failed to connect: $err'
-    	})
-	}
-
-	url := redis.get(short_id) or {
+pub fn (mut app App) redirect_url(mut ctx Context, short_id string) veb.Result {
+	url := app.redis.get(short_id) or {
 	 	return ctx.json(ShortenResponse{
 			error: 'Failed to fetch URL in Redis: $err'
-		})
-	}
-
-	redis.close() or {
-		return ctx.json(ShortenResponse{
-			error: 'Failed to close Redis connection: $err'
 		})
 	}
 
@@ -98,8 +76,14 @@ pub fn (app &App) index(mut ctx Context) veb.Result {
 }
 
 fn main() {
+	mut redis := vredis.new_client(host: '127.0.0.1', port: 6379) or {
+		panic(err)
+	}
+
 	mut app := &App{
 		domain: 'http://localhost:8000/'
+		redis: redis
 	}
+
 	veb.run[App, Context](mut app, 8000)
 }
